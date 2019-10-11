c20ec50cc27dbd6a3dd1737042648afeb03272e3
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-11 13:01:48 +0000

    build based on d6b2e8c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#creat_doc.jl-Documentation-1","page":"creat_doc.jl Documentation","title":"creat_doc.jl Documentation","text":"","category":"section"},{"location":"#","page":"creat_doc.jl Documentation","title":"creat_doc.jl Documentation","text":"greet()","category":"page"}]
+}
