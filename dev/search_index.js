2bc4369ef9dca75a77a14dde1d3f92db04e185df
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-11 13:10:39 +0000

    build based on 3104e47

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#creat_doc.jl-Documentation-1","page":"creat_doc.jl Documentation","title":"creat_doc.jl Documentation","text":"","category":"section"},{"location":"#","page":"creat_doc.jl Documentation","title":"creat_doc.jl Documentation","text":"greet()","category":"page"}]
+[{"location":"#PartiallySeparable.jl-Documentation-1","page":"PartiallySeparable.jl Documentation","title":"PartiallySeparable.jl Documentation","text":"","category":"section"},{"location":"#","page":"PartiallySeparable.jl Documentation","title":"PartiallySeparable.jl Documentation","text":"greet()","category":"page"}]
 }
