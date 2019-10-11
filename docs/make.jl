9b333ae50ae2dbd52e12d027aeca3b1988932d15
paul raynaud <paul.raynaud66@hotmail.fr>
2019-10-11 14:45:03 +0200

    modification du make.jl pour la doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,9 @@
 using Documenter
 
 
-makedocs(modules = [hello_world],
+makedocs(modules = [PartiallySeparableStructure],
 	 sitename="PartiallySeparableStructure.jl",
 	 format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
 )
 
 deploydocs(repo = "github.com/paraynaud/PartiallySeparableStructure.jl.git")
-
-
