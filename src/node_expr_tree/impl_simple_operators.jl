59f91cd0e13e354745192279b18e3bfcd13241ce
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2019-11-28 15:33:52 +0100

    modification d'un bug dans la récupération d'un type d'un arbre, ajout de tests en conséquence

--- src/node_expr_tree/impl_simple_operators.jl
+++ src/node_expr_tree/impl_simple_operators.jl
@@ -3,7 +3,7 @@ module operators
     import ..abstract_expr_node.ab_ex_nd, ..abstract_expr_node.create_node_expr
 
     import ..interface_expr_node._node_is_plus, ..interface_expr_node._node_is_minus, ..interface_expr_node._node_is_power, ..interface_expr_node._node_is_times
-    import  ..interface_expr_node._node_is_constant, ..interface_expr_node._node_is_variable,..interface_expr_node._node_is_operator
+    import ..interface_expr_node._node_is_constant, ..interface_expr_node._node_is_variable,..interface_expr_node._node_is_operator
     import ..interface_expr_node._node_is_sin, ..interface_expr_node._node_is_cos, ..interface_expr_node._node_is_tan
 
     import ..implementation_type_expr.t_type_expr_basic
@@ -43,11 +43,10 @@ module operators
         elseif _node_is_tan(op) || _node_is_cos(op) || _node_is_sin(op)
             if length(type_ch) == 1
                 t_child = type_ch[1]
-                @show _node_is_constant(t_child)
-                if _node_is_constant(t_child)
+                @show trait_type_expr._is_constant(t_child)
+                if trait_type_expr._is_constant(t_child)
                     return t_child
-                else
-                    @show "coucou"
+                else                    
                     return t_type_expr_basic(3)
                 end
             else
--- src/node_expr_tree/ordered_include.jl
+++ src/node_expr_tree/ordered_include.jl
@@ -4,9 +4,9 @@ include("itf_expr_node.jl")
 include("abs_expr_node.jl")
 
 include("impl_constants.jl")
+include("impl_variables.jl")
 include("impl_simple_operators.jl")
 include("impl_complex_operators.jl")
-include("impl_variables.jl")
 
 
 include("tr_expr_node.jl")
