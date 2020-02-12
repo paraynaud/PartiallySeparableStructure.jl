c978ff30f305d385f5c0dd634d8e8dbc2166c967
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-02-12 17:31:41 +0100

    nouveau test

--- src/quasi_newton.jl
+++ src/quasi_newton.jl
@@ -3,7 +3,7 @@ module Quasi_Newton_update
 using Test
 using LinearAlgebra, SparseArrays
 
-using Test, BenchmarkTools, ProfileView, InteractiveUtils
+# using Test, BenchmarkTools, ProfileView, InteractiveUtils
 
 
 """
@@ -75,32 +75,32 @@ to the BFGS update method.
 
 
 # exemple de code montrant les limites numériques
-x = Vector{Float64}([1, 1])
-x_1 = Vector{Float64}([2, 2])
-g = Vector{Float64}([1, 1])
-g_1 = Vector{Float64}([1, 1.000000000000001])
-B = Array{Float64,2}([2 0 ; 0 2])
-B_1 = Array{Float64,2}(undef,2,2)
-# update_SR1!(x ,x_1, g, g_1, B, B_1)
-B_BFGS = update_BFGS!(x ,x_1, g, g_1, B, B_1)
-B_SR1 = update_SR1!(x ,x_1, g, g_1, B, B_1)
-
-
-n = 5
-x_SR1_0 = rand(n)
-x_SR1_1 = rand(n)
-g_SR1_0 = rand(n)
-g_SR1_1 = rand(n)
-B_SR1_0 = Array(sparse([1:n;], [1:n;], ones(n)))
-B_SR1_1 = Array{Float64,2}(undef,n,n)
-
-update_SR1!(x_SR1_0 , x_SR1_1, g_SR1_0, g_SR1_1, B_SR1_0, B_SR1_1)
-
-@testset "tests des résultats sur les approximations simple" begin
- @test norm(B_SR1 * (x_1 - x) - (g_1-g),2) < 10^-5
- @test norm(B_BFGS * (x_1 - x) - (g_1-g),2) < 10^-5
- @test norm( B_SR1_1 * (x_SR1_1 - x_SR1_0) - (g_SR1_1 - g_SR1_0) ) < 10^-5
-end
+# x = Vector{Float64}([1, 1])
+# x_1 = Vector{Float64}([2, 2])
+# g = Vector{Float64}([1, 1])
+# g_1 = Vector{Float64}([1, 1.000000000000001])
+# B = Array{Float64,2}([2 0 ; 0 2])
+# B_1 = Array{Float64,2}(undef,2,2)
+# # update_SR1!(x ,x_1, g, g_1, B, B_1)
+# B_BFGS = update_BFGS!(x ,x_1, g, g_1, B, B_1)
+# B_SR1 = update_SR1!(x ,x_1, g, g_1, B, B_1)
+#
+#
+# n = 5
+# x_SR1_0 = rand(n)
+# x_SR1_1 = rand(n)
+# g_SR1_0 = rand(n)
+# g_SR1_1 = rand(n)
+# B_SR1_0 = Array(sparse([1:n;], [1:n;], ones(n)))
+# B_SR1_1 = Array{Float64,2}(undef,n,n)
+#
+# update_SR1!(x_SR1_0 , x_SR1_1, g_SR1_0, g_SR1_1, B_SR1_0, B_SR1_1)
+#
+# @testset "tests des résultats sur les approximations simple" begin
+#  @test norm(B_SR1 * (x_1 - x) - (g_1-g),2) < 10^-5
+#  @test norm(B_BFGS * (x_1 - x) - (g_1-g),2) < 10^-5
+#  @test norm( B_SR1_1 * (x_SR1_1 - x_SR1_0) - (g_SR1_1 - g_SR1_0) ) < 10^-5
+# end
 
 # bench_SR1_4 = @benchmark update_SR1!(x_SR1_0 , x_SR1_1, g_SR1_0, g_SR1_1, B_SR1_0, B_SR1_1)
 
