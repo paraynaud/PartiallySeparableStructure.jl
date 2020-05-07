efccc8eee914ed82c86cfac2f8ec90bcde529d27
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-05-07 20:27:37 +0200

    commit pour ]dev .

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -1,9 +1,27 @@
 # using GitHub, JSON,
 using PkgBenchmark
 
-import PartiallySeparableStructure
+using PartiallySeparableStructure
 
-commit = benchmarkpkg("PartiallySeparableStructure")  # current state of repository , script="../benchmark/benchmark.jl"
+pkg = "PartiallySeparableStructure"
+pkg = "Krylov"
+pkgid = Base.identify_package(pkg)
+pkgfile_from_pkgname = pkgid === nothing ? nothing : Base.locate_package(pkgid)
+
+if pkgfile_from_pkgname===nothing
+    if isdir(pkg)
+        pkgdir = pkg
+    else
+        error("No package '$pkg' found.")
+    end
+else
+    pkgdir = normpath(joinpath(dirname(pkgfile_from_pkgname), ".."))
+end
+@show pkgid, pkgfile_from_pkgname, pkgdir
+
+dvpt = BenchmarkConfig("dvpt")
+
+commit = benchmarkpkg("PartiallySeparableStructure", dvpt)  # current state of repository , script="../benchmark/benchmark.jl"
 # pathof(PartiallySeparableStructure) , script="/benchmark/benchmarks.jl"
 master = benchmarkpkg("PartiallySeparableStructure", "master")
 
