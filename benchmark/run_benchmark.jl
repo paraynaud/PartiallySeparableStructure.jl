2130058e0dc6ef70525815adeef8c603c69fb6e2
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-05-07 20:34:19 +0200

    commit pour ]dev . 4

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -6,10 +6,10 @@ using PartiallySeparableStructure
 pkg = "PartiallySeparableStructure"
 
 dvpt = BenchmarkConfig("dvpt")
-
+master = BenchmarkConfig("master")
 commit = benchmarkpkg("PartiallySeparableStructure", dvpt)  # current state of repository , script="../benchmark/benchmark.jl"
 # pathof(PartiallySeparableStructure) , script="/benchmark/benchmarks.jl"
-master = benchmarkpkg("PartiallySeparableStructure", "master")
+master = benchmarkpkg("PartiallySeparableStructure", master)
 
 judgement = judge(commit, master)
 
