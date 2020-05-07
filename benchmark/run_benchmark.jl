6cb9311f1104d09e0ce5c33347de7cef6beaef86
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-05-07 20:31:35 +0200

    commit pour ]dev . 3

--- benchmark/run_benchmark.jl
+++ benchmark/run_benchmark.jl
@@ -4,20 +4,6 @@ using PkgBenchmark
 using PartiallySeparableStructure
 
 pkg = "PartiallySeparableStructure"
-pkg = "Krylov"
-pkgid = Base.identify_package(pkg)
-pkgfile_from_pkgname = pkgid === nothing ? nothing : Base.locate_package(pkgid)
-
-if pkgfile_from_pkgname===nothing
-    if isdir(pkg)
-        pkgdir = pkg
-    else
-        error("No package '$pkg' found.")
-    end
-else
-    pkgdir = normpath(joinpath(dirname(pkgfile_from_pkgname), ".."))
-end
-@show pkgid, pkgfile_from_pkgname, pkgdir
 
 dvpt = BenchmarkConfig("dvpt")
 
