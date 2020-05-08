e3f481cfe3f8d88299906c6e6002b16eed7d8589
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-05-08 11:54:31 +0200

    test nouveau script 4

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -26,12 +26,12 @@ for p in problems
   SUITE["SPS_function"]["OBJ ros $n var"] = @benchmarkable PartiallySeparableStructure.evaluate_SPS($SPS_ros, $x)
 
   #calcul du gradient sous format gradient élémentaire
-  f = (y :: PartiallySeparableStructure.element_function{implementation_expr_tree.t_expr_tree} -> PartiallySeparableStructure.element_gradient{typeof(x[1])}(Vector{typeof(x[1])}(zeros(typeof(x[1]), length(y.used_variable)) )) )
+  f = (y :: PartiallySeparableStructure.element_function -> PartiallySeparableStructure.element_gradient{typeof(x[1])}(Vector{typeof(x[1])}(zeros(typeof(x[1]), length(y.used_variable)) )) )
   grad = PartiallySeparableStructure.grad_vector{typeof(x[1])}( f.(SPS.structure) )
   SUITE["SPS_function"]["grad ros $n var"] = @benchmarkable PartiallySeparableStructure.evaluate_SPS_gradient!($SPS_ros, $x, $grad)
 
   #calcul du Hessien
-  f = ( elm_fun :: PartiallySeparableStructure.element_function{implementation_expr_tree.t_expr_tree} -> PartiallySeparableStructure.element_hessian{Float64}( Array{Float64,2}(undef, length(elm_fun.used_variable), length(elm_fun.used_variable) )) )
+  f = ( elm_fun :: PartiallySeparableStructure.element_function{} -> PartiallySeparableStructure.element_hessian{Float64}( Array{Float64,2}(undef, length(elm_fun.used_variable), length(elm_fun.used_variable) )) )
   t = f.(SPS2.structure) :: Vector{PartiallySeparableStructure.element_hessian{typeof(x[1])}}
   H = PartiallySeparableStructure.Hess_matrix{typeof(x[1])}(t)
 
