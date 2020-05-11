2c88e21f89874f8626e5ec9bf648fc84b2fc500c
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-05-11 22:44:49 +0200

    modif script test ajout run_solver_benchmark.jl 5

--- benchmark/solver_benchmark.jl
+++ benchmark/solver_benchmark.jl
@@ -31,12 +31,12 @@ SUITE["L-SR1"] = BenchmarkGroup()
 SUITE["P-SR1"] = BenchmarkGroup()
 SUITE["P-BFGS"] = BenchmarkGroup()
 
-atol = 1.0e-5
-rtol = 1.0e-6
-max_time = 300.0
-max_eval = 5000
-
 for i in 1:length(problems)
+
+  atol = 1.0e-5
+  rtol = 1.0e-6
+  max_time = 300.0
+  max_eval = 5000
   (m_ros, evaluator, obj_ros) = problems[i]
   obj_ros_expr_tree = PartiallySeparableStructure.expr_tree_from_Expr(obj_ros)
   n = m_ros.moi_backend.model_cache.model.num_variables_created
@@ -44,11 +44,11 @@ for i in 1:length(problems)
 
   # SPS_ros = PartiallySeparableStructure.deduct_partially_separable_structure(obj_ros_expr_tree, n)
 
-  SUITE["Trunk"]["ros $n var"] = @benchmarkable JSOSolvers.trunk(&prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
-  SUITE["Trunk_LSR1"]["ros $n var"] = @benchmarkable JSOSolvers.trunk(NLPModels.LSR1Model(prob); max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
-  SUITE["L-BFGS"]["ros $n var"] = @benchmarkable my_LBFGS(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
-  SUITE["L-SR1"]["ros $n var"] = @benchmarkable my_LSR1(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
-  SUITE["P-BFGS"]["ros $n var"] = @benchmarkable PartiallySeparableStructure.solver_TR_PBFGS!(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
-  SUITE["P-SR1"]["ros $n var"] = @benchmarkable PartiallySeparableStructure.solver_TR_PSR1!(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
+  SUITE["Trunk"]["ros $n var"] = @benchmarkable JSOSolvers.trunk(prob; max_time=max_time, max_eval=max_eval, atol=atol, rtol=rtol)
+  # SUITE["Trunk_LSR1"]["ros $n var"] = @benchmarkable JSOSolvers.trunk(NLPModels.LSR1Model(prob); max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
+  # SUITE["L-BFGS"]["ros $n var"] = @benchmarkable my_LBFGS(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
+  # SUITE["L-SR1"]["ros $n var"] = @benchmarkable my_LSR1(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
+  # SUITE["P-BFGS"]["ros $n var"] = @benchmarkable PartiallySeparableStructure.solver_TR_PBFGS!(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
+  # SUITE["P-SR1"]["ros $n var"] = @benchmarkable PartiallySeparableStructure.solver_TR_PSR1!(prob; max_time=&max_time, max_eval=&max_eval, atol=&atol, rtol=&rtol)
 
 end
