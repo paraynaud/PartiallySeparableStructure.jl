4eedc29785f80c7291881bc545a1892045ab422f
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-05-12 15:55:01 +0200

    modif solver_benchmark

--- benchmark/run_solver_benchmark.jl
+++ benchmark/run_solver_benchmark.jl
@@ -6,4 +6,6 @@ import PartiallySeparableStructure
 commit_solver = benchmarkpkg("PartiallySeparableStructure",script="benchmark/solver_benchmark.jl")  #dernier commit sur la branche sur laquelle on se trouve
 master_solver = benchmarkpkg("PartiallySeparableStructure", "master_dvpt", script="benchmark/solver_benchmark.jl") # branche masterjudgement_solver = judge(master_solver, commit_solver)
 # judgement = judge("PartiallySeparableStructure", "master")
+judgement_solver = judge(master_solver, commit_solver)
+
 export_markdown("benchmark/judgement_solver.md", judgement_solver)
