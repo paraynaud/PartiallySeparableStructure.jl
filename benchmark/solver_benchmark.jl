c5ac5cb89102950ac4703f1f64d579b45678b14d
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2020-05-20 11:37:39 +0200

    reduction de la taille des problemes solver_benchmark.jl

--- benchmark/solver_benchmark.jl
+++ benchmark/solver_benchmark.jl
@@ -16,9 +16,9 @@ using ..PartiallySeparableStructure
 const SUITE = BenchmarkGroup()
 
 
-n = [100,200,500,1000,2000,5000]
+# n = [100,200,500,1000,2000,5000]
 # n = [10,20,30]
-
+n = [500,1000,2000,4000]
 problems = create_Rosenbrock_JuMP_Model.(n)
 nlp_problems = MathOptNLPModel.([p[1] for p in problems])
 
